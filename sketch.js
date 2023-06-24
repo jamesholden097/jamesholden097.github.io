f9ad8eae265d5092ab12da4e72c19c2161c4c505
jamesholden097 <117689818+jamesholden097@users.noreply.github.com>
2023-06-25 02:44:47 +0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,59 @@
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/074-clock.html
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  angleMode(DEGREES);
+}
+
 function draw() {
-    background(237, 34, 93);
-    fill(0);
-  
-    if (mouseIsPressed === true) {
-      ellipse(50, 50, 50, 50);
-    } else {
-      rect(25, 25, 50, 50);
-    }
-  
-    print(mouseIsPressed);
-    describe(`black 50-by-50 rect becomes ellipse with mouse click/press.
-      fuchsia background.`);
-  }
+  background(0);
+  translate(200, 200);
+  rotate(-90);
+
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+
+  strokeWeight(8);
+  stroke(255, 100, 150);
+  noFill();
+  let secondAngle = map(sc, 0, 60, 0, 360);
+  arc(0, 0, 300, 300, 0, secondAngle);
+
+  stroke(150, 100, 255);
+  let minuteAngle = map(mn, 0, 60, 0, 360);
+  arc(0, 0, 280, 280, 0, minuteAngle);
+
+  stroke(150, 255, 100);
+  let hourAngle = map(hr % 12, 0, 12, 0, 360);
+  arc(0, 0, 260, 260, 0, hourAngle);
+
+  push();
+  rotate(secondAngle);
+  stroke(255, 100, 150);
+  line(0, 0, 100, 0);
+  pop();
+
+  push();
+  rotate(minuteAngle);
+  stroke(150, 100, 255);
+  line(0, 0, 75, 0);
+  pop();
+
+  push();
+  rotate(hourAngle);
+  stroke(150, 255, 100);
+  line(0, 0, 50, 0);
+  pop();
+
+  stroke(255);
+  point(0, 0);
+
+
+  //  fill(255);
+  //  noStroke();
+  //  text(hr + ':' + mn + ':' + sc, 10, 200);
+
+
+}
